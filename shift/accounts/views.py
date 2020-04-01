22c4676cd95b62612057dbe49ad5ebc1716412fc
murakamikoudai <kodaimura123@gmail.com>
2020-04-01 16:57:23 +0900

    all ok

--- shift/accounts/views.py
+++ shift/accounts/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
 from .forms import LoginForm, ProfileForm, DaysForm
-from .models import Profile
 
 
 class Login(LoginView):
@@ -15,83 +14,76 @@ class Logout(LoginRequiredMixin, LogoutView):
     """ログアウトページ"""
     template_name = 'accounts/login.html'
 
-    
+
 def signup(request):
     user_form = UserCreationForm(request.POST)
     profile_form = ProfileForm(request.POST)  #{{ form.as_p }}の部分で表示される
     if request.method == 'POST' and user_form.is_valid() and profile_form.is_valid():
-            
+
         user = user_form.save(commit=False)
         user.is_active = True
         user.save()
-        
+
         profile = profile_form.save(commit=False)
         profile.user = user
         profile.save()
         return redirect('../login/')
-        
+
     context = {
         "user_form": user_form,
         "profile_form": profile_form,
     }
-        
+
     return render(request, 'accounts/signup.html', context)
-    
-    
+
+
 
 def days_choice(request):
     days = ""
     days_late = ""
+    state = ""
+    state_late = ""
 
-    state = Profile.objects.get(id=request.user.id).days
-    state_late = Profile.objects.get(id=request.user.id).days_late
+    if not request.user.profile.days in [None, '0']:
+        state = request.user.profile.days
+    if not request.user.profile.days_late in [None, '0']:
+        state_late = request.user.profile.days_late
 
     form = DaysForm(request.POST or None,initial={'days': state, 'days_late':state_late})
-    
+
     if request.method == 'POST' and form.is_valid():
-        obj = Profile.objects.get(id=request.user.id)
+        obj = request.user.profile
         form = DaysForm(request.POST, instance = obj)
-        
+
         form.save(commit = False)
-        
-        days = "定刻出勤可を"+str(form.cleaned_data['days'])+"日に変更"
-        days_late = "遅れ出勤可を"+str(form.cleaned_data['days_late'])+"日に変更"
-        
-        
+
+        days = "定刻出勤可を"+str(form.cleaned_data['days'])+"日で登録"
+        days_late = "遅れ出勤可を"+str(form.cleaned_data['days_late'])+"日で登録"
+
+
         if not (dayscheck(form.cleaned_data['days'],form.cleaned_data['days_late'])):
             days = "登録できませんでした。"
             days_late = "形式の間違い、または日付の重複があります。"
-                        
+
         else:
             if form.cleaned_data['days'] == None:
                 form.days = state
                 days = ""
-                
+
             if form.cleaned_data['days_late'] == None:
                 form.days_late = state_late
                 days_late = ""
-    
+
             form.save()
-                
-    state = ""
-    state_late = ""
-    
-    if not request.user.profile.days in [None, '0']:
-        state = request.user.profile.days
-    if not request.user.profile.days in [None, '0']:
-        state_late = request.user.profile.days_late
-    
+
     params = {
         'form': form,
         'name': request.user.profile.name,
         'days': days,
         'days_late': days_late,
-        'state': state,
-        'state_late': state_late,
-
     }
     return render(request, 'accounts/dayschoice.html', params)
-        
+
 
 
 def dayscheck(days, days_late):
@@ -100,7 +92,7 @@ def dayscheck(days, days_late):
     d10 = 0
     if days == None and days_late == None:
         return True
-    
+
     if days == None:
         pass
     else:
@@ -131,7 +123,7 @@ def dayscheck(days, days_late):
             notls.append(d10 * 10 + d1)
             d1 = 0
             d10 = 0
-        
+
     if days_late == None:
         pass
     else:
@@ -161,5 +153,5 @@ def dayscheck(days, days_late):
                 return False
 
     return True
-            
-            
+
+
--- shift/shiftapp/views.py
+++ shift/shiftapp/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from .forms import ResetForm
-from accounts.views import Login, Logout, signup
 from accounts.models import Profile
 from django.db.models import Q
-import numpy as np
+#import numpy as np
 import random
 
 import datetime
@@ -21,18 +20,18 @@ def yoshico(request):
                 d.days = None
                 d.days_late = None
                 d.save()
-            
+
     return render(request, 'shiftapp/yoshico.html', {'data': data, 'f': f, })
-    
+
 def num_to_list (num):
     return [num]
 
 #誰がいつ入れるかのリストを返す。
 
 def make_baseshift(data):
-   
+
     ls = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
-    
+
     for d in data:
         name = d.name
         daysls = days_to_list(d.days)
@@ -41,18 +40,18 @@ def make_baseshift(data):
             ls[day].append(name)
         for day in daysls_late:
             ls[day].append('*'+ name)
-        
+
     return ls
-    
+
 #days 1.2.3.4.5 → [1,2,3,4,5]
 def days_to_list(days):
     d1 = 0
     d10 = 0
     ls = []
-    
+
     if days == None:
         return []
-    
+
     for day in days:
         if day.isdecimal() and day.isalnum():
             if int(d1) == 0:
@@ -60,17 +59,17 @@ def days_to_list(days):
             else:
                 d10 = d1
                 d1 = int(day)
-            
+
         elif day == '.' or day == ',':
             if d10 * 10 + d1 != 0 and not (d10 * 10 + d1 in ls):
                 ls.append(d10 * 10 + d1)
-            
+
             d1 = 0
             d10 = 0
-        
+
     if d10 * 10 + d1 != 0 and not (d10 * 10 + d1 in ls):
         ls.append(d10 * 10 + d1)
-        
+
     return ls
 
 #["a","b","c","d"]→ [["a","b"],["a","c"],["a","d"], , , ,]
@@ -95,7 +94,7 @@ def member_to_pair(member, dic):
                     lS.append([m1, m2])
                 else:
                     ls.append([m1, m2])
-                    
+
     if LS != []:
         return LS
     elif Ls != []:
@@ -104,7 +103,7 @@ def member_to_pair(member, dic):
         return lS
     else:
         return ls
-    
+
 def var(ls):
     mean = sum(ls) / len(ls)
     s = 0
@@ -123,9 +122,9 @@ def eval(shift, member_list):
         late_count += n_late
         nls.append(n + n_late)
         nls2.append(member + ':'+ str(n + n_late))
-    return - np.var(nls) - late_count, nls2
-        
-        
+    return - var(nls) - late_count, nls2
+
+
 def choiceshift(basels, member_list):
     maxeval = -1000000
     for i in range(50000):
@@ -142,15 +141,15 @@ def choiceshift(basels, member_list):
             bnls = nls
 
     return best_shift, bnls
-    
-            
-            
+
+
+
 def makeshift(request):
     date = datetime.date.today()
     next_month = date.replace(day=1) + relativedelta(months=1)
     cal.setfirstweekday(6)
     next_month_cal = cal.monthcalendar(next_month.year, next_month.month)
-    
+
     basecal = list(itertools.chain.from_iterable(next_month_cal)) #一次元
     daysrange = max(basecal)
     wedls = []
@@ -182,14 +181,14 @@ def makeshift(request):
     baseshift[0] = []
     for i in wedls:
         baseshift[i] = []
-    
+
     for bs, i in zip(baseshift, range(len(baseshift))):
         if i > daysrange:
             baseshift[i] = []
         else:
             baseshift[i] = member_to_pair(bs, gender_dic)
     best_shift, nls = choiceshift(baseshift, member_list)
-    
+
     for calday, i in zip(listcal, range(len(listcal))):
         if listcal[i] == [0]:
             listcal[i] = ""
@@ -203,9 +202,11 @@ def makeshift(request):
 
     shift = [listcal[i:i+7] for i in range(0,len(listcal),7)]
     param = {
+        'month' : next_month.month,
         'shift' : shift,
         'nls' : nls,
     }
-    
+
     return render(request, 'shiftapp/shift.html', param)
-    
+
+
